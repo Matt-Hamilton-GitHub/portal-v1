873f84fbcd3d7310c86c23fc4e1c485d4ecb1dbb
Matt <i.w.b.s.bilm@gmail.com>
2023-08-29 11:54:53 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TopSection from './components/TopSection';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="progressbar"></div>
+      <div id="scrollPath"></div>
+      <TopSection />
+    </>
   );
 }
 
